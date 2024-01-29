2fee0ce067b2c85c661da211c279d783d160f942
anikeshaman <aaj020@nist.edu>
2024-01-29 12:16:01 +0530

    initial

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -123,27 +123,6 @@ const Login = () => {
       >
         Guest User Login
       </Button>
-
-      <Button
-        //fontWeight="bold"
-        variant="solid"
-        colorScheme="cyan"
-        width="100%"
-        onClick={() => {
-          //console.log("video");
-          //navigate("/https://dev.to/siddharthssb11")
-        }}
-      >
-        <a
-          href="https://www.loom.com/share/339d7abfe1924bbd8cb53c0cc3fac551"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="Demo-Video"
-          width="100%"
-        >
-          <strong>Demo&nbsp;📽️&nbsp;Video</strong>, Recommended <strong><u>2x SPEED</u></strong>&nbsp;&#10132;
-        </a>
-      </Button>
     </VStack>
   );
 };
